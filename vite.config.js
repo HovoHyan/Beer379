ddc0762890bd973fc5eb769b5957498718a9ac24
Hovo Harutyunyan <hovohyan16@gmail.com>
2025-09-10 16:38:19 +0400

    For deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/LandingPage_Beer",
+  base: process.env.GITHUB_PAGES ? "/LandingPage_Beer/" : "/",
 });
